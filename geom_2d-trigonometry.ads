4896f6e049a8771e3632c97691b3cb93774ec98c
m0 <m0@viper.internal.network>
2009-10-15 20:33:51 +0100

    Update type definition.

--- geom_2d-trigonometry.ads
+++ geom_2d-trigonometry.ads
@@ -1,3 +1,4 @@
+with Ada.Numerics;
 with Geom_2D.Types;
 
 generic
@@ -5,8 +6,10 @@ generic
 
 package Geom_2D.Trigonometry is
 
-  type Degrees_t is new Types.Real_Type'Base;
-  type Radians_t is new Types.Real_Type'Base;
+  use type Types.Real_Type'Base;
+
+  type Degrees_t is new Types.Real_Type'Base range 0.0 .. 360.0;
+  type Radians_t is new Types.Real_Type'Base range 0.0 .. 360.0 * (Ada.Numerics.Pi / 180.0);
 
   function To_Radians (Degrees : in Degrees_t) return Radians_t;
   pragma Inline (To_Radians);
